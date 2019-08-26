22d987a82513a6cc9e46ed8d784eb7a5dac686a9
nobalpreet <n.nobalpreet@accenture.com>
2019-08-26 13:24:52 -0400

    Initial commit

---
+++ app/src/main/java/org/queens/MainActivity.java
@@ -0,0 +1,14 @@
+package org.queens;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //AppCenter.start(getApplication(), "Hello App", Analytics.class, Crashes.class);
+    }
+}
