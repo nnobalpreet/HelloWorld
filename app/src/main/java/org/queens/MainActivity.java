c3743ea7f3c783bad7be07b447661bbff8bdbc37
Nobalpreet Nobalpreet <39172933+nnobalpreet@users.noreply.github.com>
2019-08-27 10:44:00 -0400

    Update MainActivity.java

--- app/src/main/java/org/queens/MainActivity.java
+++ app/src/main/java/org/queens/MainActivity.java
@@ -3,6 +3,9 @@ package org.queens;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //AppCenter.start(getApplication(), "Hello App", Analytics.class, Crashes.class);
+        AppCenter.start(getApplication(), "8034619f-95fc-4c57-8d09-eb1f0755a910",
+                  Analytics.class, Crashes.class);
     }
 }
